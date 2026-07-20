340104aad50783bc7263eb10799e988d112def4f
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-20 14:21:04 -0400

    Version 2.59-2.62: Add services with inject() for dependency injection, remove event emission pattern and use direct service calls

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, Output, EventEmitter, signal } from '@angular/core';
+import { Component, Output, EventEmitter, Input, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 export interface newTask {
   title: string;
@@ -16,22 +17,24 @@ export interface newTask {
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>(); // add newTask as the type of data being emitted
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
-  onCancel() {
-    this.cancel.emit();
+  private tasksService = inject(TasksService);
+
+  onClose() {
+    this.close.emit();
   }
   
   onSubmit() {
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDate
-    });
+    }, this.userId);
+    this.close.emit();
   }
 }
--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'; 
+import { Component, Input, inject } from '@angular/core'; 
 import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
@@ -11,12 +12,10 @@ import { CardComponent } from "../../shared/card/card.component";
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
   @Input({required: true}) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+  private tasksService = inject(TasksService);
 
   onCompleteTask(){
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
-
 }
