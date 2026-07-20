72796af99d61b5ba4ac8431699fcccb2ef070dc6
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-20 12:01:52 -0400

    Version 2.57-2.58: Add content projection with ng-content directive and date pipe formatting

---
+++ 01-starting-project/src/app/shared/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+}
+
+
+// Using card component as a wrapper around user button and task component
