f7e1f57f155adf59e0ca87118389ca7b34a82287
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-21 12:07:22 -0400

    Version 4.78-4.80: Implement user input form with ngSubmit and two-way binding to extract form values

--- 04-starting-project/src/app/user-input/user-input.component.ts
+++ 04-starting-project/src/app/user-input/user-input.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
 
+  enteredInitialInvestment = '0'; // setting to string to match input type, which is always a string
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5'; // default 5% return rate 
+  enteredDuration = '10'; // default 10 years
+
+  onSubmit() {
+    console.log('Form submitted');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
+  }
+  
+  
 }
