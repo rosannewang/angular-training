f8bbf567bf4ab2b6acdc93d961ead7192ec110bd
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-23 14:00:39 -0400

    Version 6.98-6.103: Create reusable dashboard components with inputs and content projection

---
+++ 06-starting-project/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+
+  // alternatively
+  // @Input({required: true}) image!: { src: string; alt: string }; // We are expecting an image object; ! = never undefined
+  // @Input({required: true}) title!: string;
+  
+  // signals approach
+  image = input.required<{ src: string; alt: string }>() 
+  title = input.required<string>()
+
+}
