389215547906ebdbbfba9a40b808fd9e620db3f1
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-24 14:44:17 -0400

    Version 6.117-6.122: Add dynamic class binding and setInterval for random status changes

--- 06-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ 06-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+
+  // Trying to randomly change the status
+  constructor() {
+    setInterval(() => { 
+      const rnd = Math.random(); // 0-0.999...
+
+      const randomStatus = rnd > 0.5 ? 'online' : 'offline';
+      if(rnd < 0.5){
+        this.currentStatus = 'offline';
+      } else if(rnd < 0.9){
+        this.currentStatus = 'online';
+      } else{
+        this.currentStatus = 'unknown';
+      }
+
+    }, 5000); // set interval runs every 5000 ms (5 seconds)
+  }
 }
