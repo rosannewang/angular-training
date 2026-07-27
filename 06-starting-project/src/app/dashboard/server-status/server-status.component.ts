2168d1ecf4c572092cad641d20483c1708ed0093
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-27 11:15:44 -0400

    Version 6.126: Add lifecycle hooks (OnInit, AfterViewInit, OnDestroy) to ServerStatusComponent

--- 06-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ 06-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  // private interval?: NodeJS.Timeout
+  private interval?: ReturnType<typeof setInterval> // removes Cannot find name 'NodeJS' Error
+
+  constructor() {}
 
   // Trying to randomly change the status
   ngOnInit(){
+    console.log('ON INIT')
+    this.interval = setInterval(() => {
     const rnd = Math.random();
     if (rnd < 0.5) {
       this.currentStatus = 'offline';
@@ -20,5 +26,14 @@ export class ServerStatusComponent implements OnInit {
     } else {
       this.currentStatus = 'unknown';
     }
+    }, 5000);
   } // note: this method won't trigger errors, so it's recommended to add 'implements OnInit' in the class
+  
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT')
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
+  }
 }
--- 06-starting-project/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 06-starting-project/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
-  imports: [],
+  imports: [ControlComponent],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
 })
