1c56d2890850c821c5091e6a61a769b42847f326
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-28 09:49:44 -0400

    Version 6.149: Use model signal for two-way binding with signal syntax

--- 06-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 06-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, model } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,13 +9,15 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  @Input({required: true}) size!: {width: string; height: string};
-  @Output() sizeChange  = new EventEmitter<{width: string; height: string}>(); // Output must be named varChange 
+  // @Input({required: true}) size!: {width: string; height: string};
+  // @Output() sizeChange  = new EventEmitter<{width: string; height: string}>(); // Output must be named varChange 
+
+  size = model.required<{width: string; height: string}>(); // size is a model signal
 
   onReset() {
-    this.sizeChange.emit({
+    this.size.set({
       width: '200',
       height: '200'
-    }); // the parameters are what we want to reset the rectangle to
+    })
   }
 }
