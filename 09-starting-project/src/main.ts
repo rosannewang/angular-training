d75d62c4c71058e3fcd44dab4094c12bbbbf4656
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-29 12:04:29 -0400

    Version 9.182-9.184: Implement ElementInjector by moving TasksService to component providers

--- 09-starting-project/src/main.ts
+++ 09-starting-project/src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { TasksService } from './app/tasks/tasks.service';
+
+// bootstrapApplication(AppComponent, {
+//     providers: [TasksService] // EnvironmentInjector; does not allow for tree shaping
+// }).catch((err) => console.error(err));
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
