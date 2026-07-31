1f8c197c81b7b525f197f7639d9de417362e8fdb
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-31 12:53:00 -0400

    Version 11.212-11.213: Add computed signals and interval with signals

--- 11-starting-project/src/app/app.component.ts
+++ 11-starting-project/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, computed, effect, inject, signal } from '@angular/core';
 import { interval, map } from 'rxjs';
 
 
@@ -8,10 +8,28 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit{
-
+  clickCount = signal(0);
+  interval = signal(0);
+  doubleInterval = computed(() => this.interval() * 2);
+  message = computed(() => `Interval: ${this.interval()}, Double Interval: ${this.doubleInterval()}`);
   private destroyRef = inject(DestroyRef);
+
+
+  constructor(){
+    effect(() => { // subscription set up for you
+      console.log(`Clicked button ${this.clickCount()} times`);
+    });
+  }
   
   ngOnInit(): void {
+    setInterval(() => {
+      this.interval.update(prevIntevalNumber => prevIntevalNumber + 1);
+    }, 1000);
+
+    effect(() => {
+      console.log(this.message()); // no subscription needed
+    });
+
     // automatically produces an observable that emits a number every second
     // you need at least 1 subscriber to start emitting values
     const subscription = interval(1000).pipe(
@@ -26,4 +44,7 @@ export class AppComponent implements OnInit{
     });
   }
 
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1);
+  }
 }
