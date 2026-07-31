ae9d702b5125e5a4ad742a635eba8b9178d2823f
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-31 12:33:56 -0400

    Version 11.210: Implement interval observable with subscription management

--- 11-starting-project/src/app/app.component.ts
+++ 11-starting-project/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  private destroyRef = inject(DestroyRef);
+  
+  ngOnInit(): void {
+    // automatically produces an observable that emits a number every second
+    // you need at least 1 subscriber to start emitting values
+    const subscription = interval(1000).subscribe({ // counts up from 0
+      next: (val) => console.log(val), // triggered for every new value
+      // error: () => {} // triggered if there's an error; useful for HTTP requests
+    }); 
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 
 }
