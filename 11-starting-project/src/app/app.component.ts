b4adf606b3dde71536921a7333824ba5748950aa
Rosanne Wang <rosanne7wang@gmail.com>
2026-07-31 12:57:53 -0400

    Version 11.214: Convert signal to observable using toObservable

--- 11-starting-project/src/app/app.component.ts
+++ 11-starting-project/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, DestroyRef, OnInit, computed, effect, inject, signal } from '@angular/core';
-import { interval, map } from 'rxjs';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { interval, map, Observable } from 'rxjs';
+
 
 
 @Component({
@@ -9,38 +11,42 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit{
   clickCount = signal(0);
-  interval = signal(0);
-  doubleInterval = computed(() => this.interval() * 2);
-  message = computed(() => `Interval: ${this.interval()}, Double Interval: ${this.doubleInterval()}`);
+  clickCount$ = toObservable(this.clickCount);
+  // interval = signal(0);
+  // doubleInterval = computed(() => this.interval() * 2);
+  // message = computed(() => `Interval: ${this.interval()}, Double Interval: ${this.doubleInterval()}`);
   private destroyRef = inject(DestroyRef);
 
 
   constructor(){
-    effect(() => { // subscription set up for you
-      console.log(`Clicked button ${this.clickCount()} times`);
-    });
+    // effect(() => { // subscription set up for you
+    //   console.log(`Clicked button ${this.clickCount()} times`);
+    // });
   }
   
   ngOnInit(): void {
-    setInterval(() => {
-      this.interval.update(prevIntevalNumber => prevIntevalNumber + 1);
-    }, 1000);
+    // setInterval(() => {
+    //   this.interval.update(prevIntevalNumber => prevIntevalNumber + 1);
+    // }, 1000);
 
-    effect(() => {
-      console.log(this.message()); // no subscription needed
-    });
+    // effect(() => {
+    //   console.log(this.message()); // no subscription needed
+    // });
 
-    // automatically produces an observable that emits a number every second
-    // you need at least 1 subscriber to start emitting values
-    const subscription = interval(1000).pipe(
-      map((val) => val * 2),
-      // add more operators as needed here
-    ).subscribe({ // counts up from 0
-      next: (val) => console.log(val), // triggered for every new value
-      // error: () => {} // triggered if there's an error; useful for HTTP requests
-    }); 
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
+    // // automatically produces an observable that emits a number every second
+    // // you need at least 1 subscriber to start emitting values
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 2),
+    //   // add more operators as needed here
+    // ).subscribe({ // counts up from 0
+    //   next: (val) => console.log(val), // triggered for every new value
+    //   // error: () => {} // triggered if there's an error; useful for HTTP requests
+    // }); 
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+    this.clickCount$.subscribe({
+      next: (val) => console.log(`Clicked button ${this.clickCount()} times`),
     });
   }
 
