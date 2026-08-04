b4eeef8f156f5dc109a40d4ff09d2a03da00a4dc
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-04 09:48:20 -0400

    Version 12.220-12.224: Implement HTTP requests and fetch available places

--- 12-starting-project/src/app/places/available-places/available-places.component.ts
+++ 12-starting-project/src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -11,6 +12,24 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient); // null error on its own, so we need to provide it in the app config
+  private destroyRef = inject(DestroyRef);
+  // alternatively use a module or constructor:
+  // constructor(private httpCleint: HttpClient) {}
+
+  ngOnInit() {
+    const subscription = this.httpClient
+    .get<{ places: Place[] }>('http://localhost:3000/places')
+    .subscribe({
+      next: (resData) => {
+      console.log(resData.places)
+      }
+    });
+
+    this.destroyRef.onDestroy (() => {
+        subscription.unsubscribe()
+    });
+  }
 }
