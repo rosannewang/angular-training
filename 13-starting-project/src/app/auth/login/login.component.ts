d4132ed0c141df81bc25ccff403be9ab6b72778a
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-05 16:49:54 -0400

    Version 13.254: Add validators and error messages to reactive form

--- 13-starting-project/src/app/auth/login/login.component.ts
+++ 13-starting-project/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +12,26 @@ export class LoginComponent {
 
   // 1. Set up the form
   form = new FormGroup({ // register key value pairs where each value is a FormControl
-    email: new FormControl(''),
-    password: new FormControl('')
+    email: new FormControl('', {
+      validators: [ Validators.required, Validators.email ],
+
+    }),
+    password: new FormControl('', {
+      validators: [ Validators.required, Validators.minLength(6) ]
+    })
   }); 
+
+  get emailIsInvalid(){
+    return this.form.controls.email.touched 
+      && this.form.controls.email.dirty 
+      && this.form.controls.email.invalid;
+  }
+  
+  get passwordIsInvalid(){
+    return this.form.controls.password.touched 
+      && this.form.controls.password.dirty 
+      && this.form.controls.password.invalid;
+  }
 
   onSubmit() {
     console.log(this.form);
