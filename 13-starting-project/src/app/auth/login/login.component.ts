dc59a88c6dedfb8ab519a7767afc71d2cf3b9f20
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-05 16:23:59 -0400

    Version 13.249: Add form value persistence with localStorage and debounceTime

--- 13-starting-project/src/app/auth/login/login.component.ts
+++ 13-starting-project/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,21 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor(){
+    afterNextRender(() => {
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => window.localStorage.setItem(
+          'saved-login-form',
+          JSON.stringify({email: value.email})),
+      });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm){
     // console.log(formData);
 
@@ -19,5 +35,8 @@ export class LoginComponent {
     const enteredPassword = formData.form.value.password;
     console.log(formData.form);
     console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset();
+    // formData.form.markAsDirty(); // lots of other functions you could use for more nuanced cases
   }
 }
