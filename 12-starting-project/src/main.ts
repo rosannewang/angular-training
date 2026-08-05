1c6bf4a715d6894342d9208b7b0ed4bda0cfeb6a
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-05 13:53:24 -0400

    Version 12.238-12.239: Add HTTP interceptor for logging outgoing requests

--- 12-starting-project/src/main.ts
+++ 12-starting-project/src/main.ts
@@ -1,9 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 
+function loggingInterceptor(
+    request: HttpRequest<unknown>, 
+    next: HttpHandlerFn
+) {
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG', 'TESTING')
+    // });
+    console.log('[Outgoing Request]');
+    console.log(request);
+    // return next(req);
+    return next(request);
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()]
+    providers: [provideHttpClient(
+        withInterceptors([loggingInterceptor]) // register all interceptors for all requests and responses
+    )]
 }).catch((err) => console.error(err));
