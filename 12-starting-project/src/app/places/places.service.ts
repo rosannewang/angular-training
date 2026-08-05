c182148d464512dbd05c0f9bcfb30a11bdda597d
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-05 13:01:17 -0400

    Version 12.233-12.235: Implement optimistic UI updates with rollback on error

--- 12-starting-project/src/app/places/places.service.ts
+++ 12-starting-project/src/app/places/places.service.ts
@@ -28,10 +28,21 @@ export class PlacesService {
     }));
   }
 
-  addPlaceToUserPlaces(placeId: string) {
+  addPlaceToUserPlaces(place: Place) {
+    const prevPlaces = this.userPlaces();
+
+    if(!prevPlaces.some(p => p.id === place.id)) { 
+      this.userPlaces.set([...prevPlaces, place]);
+    }
+    
     return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId,
-    });
+      placeId: place.id,
+    }).pipe(
+      catchError(error => {
+        this.userPlaces.set(prevPlaces)
+        return throwError(() => new Error('Failed to add place to user places.'));
+      })
+    );
   }
 
   removeUserPlace(place: Place) {}
