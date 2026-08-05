f00caf436ad3276216d749d4c239efe53f9432fb
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-05 16:44:07 -0400

    Version 13.252-13.253: Implement reactive forms with FormGroup and FormControl

--- 13-starting-project/src/app/auth/login/login.component.ts
+++ 13-starting-project/src/app/auth/login/login.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  // 1. Set up the form
+  form = new FormGroup({ // register key value pairs where each value is a FormControl
+    email: new FormControl(''),
+    password: new FormControl('')
+  }); 
+
+  onSubmit() {
+    console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail, enteredPassword);
+  }
+}
