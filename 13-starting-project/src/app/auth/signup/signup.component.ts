c1aa086cde9f5354faf6b2762c9d5add76095105
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-06 11:45:28 -0400

    Version 13.260-13.261: Add all form controls to signup component

--- 13-starting-project/src/app/auth/signup/signup.component.ts
+++ 13-starting-project/src/app/auth/signup/signup.component.ts
@@ -11,15 +11,28 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 
 export class SignupComponent {
-  
-
   form = new FormGroup({ 
     email: new FormControl('', {
       validators: [ Validators.required, Validators.email ],
     }),
     password: new FormControl('', {
       validators: [ Validators.required, Validators.minLength(6) ],
-    })
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [ Validators.required, Validators.minLength(6) ],
+    }),
+    firstName: new FormControl('', { validators: [ Validators.required ], }),
+    lastName: new FormControl('', { validators: [ Validators.required ], }),
+    street: new FormControl('', { validators: [ Validators.required ], }),
+    number: new FormControl('', { validators: [ Validators.required ], }),
+    postalCode: new FormControl('', { validators: [ Validators.required ], }),
+    city: new FormControl('', { validators: [ Validators.required ], }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', { validators: [ Validators.required ], }), // dropdown
+  
+    agree: new FormControl(false, {validators: [Validators.required]} ),
+  
   }); 
 
   onSubmit(){
