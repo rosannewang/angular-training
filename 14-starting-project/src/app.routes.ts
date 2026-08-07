805245cf4953a503bd646c01d91bdeda75118385
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-07 11:22:35 -0400

    Version 14.269-14.272: Add router outlet, update app component, and configure routes

--- 14-starting-project/src/app.routes.ts
+++ 14-starting-project/src/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './app/tasks/tasks.component';
+import { NoTaskComponent } from './app/tasks/no-task/no-task.component';
 
 export const routes: Routes = [
+    {
+        path: '', // your-domain/
+        component: NoTaskComponent,
+    },
     { 
         path: 'tasks', // your-domain/tasks
         component: TasksComponent 
--- 14-starting-project/src/app/users/user/user.component.ts
+++ 14-starting-project/src/app/users/user/user.component.ts
@@ -1,10 +1,12 @@
 import { Component, computed, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
