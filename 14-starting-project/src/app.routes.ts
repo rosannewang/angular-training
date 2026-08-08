d4a440c5e005d0c3366e6e488daa7d4d0a64df87
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-08 00:06:25 -0400

    Version 14.276-14.278: Add child routes for tasks and new task, update navigation links

--- 14-starting-project/src/app.routes.ts
+++ 14-starting-project/src/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { UserTasksComponent } from './app/users/user-tasks/user-tasks.component';
 import { NoTaskComponent } from './app/tasks/no-task/no-task.component';
+import { TasksComponent } from './app/tasks/tasks.component';
+import { NewTaskComponent } from './app/tasks/new-task/new-task.component';
 
 export const routes: Routes = [
     {
@@ -9,6 +11,16 @@ export const routes: Routes = [
     },
     { 
         path: 'users/:userId', // your-domain/users/uid
-        component: UserTasksComponent
+        component: UserTasksComponent, // add child router in user-tasks html
+        children: [
+            { 
+                path: 'tasks', // your-domain/users/uid/tasks
+                component: TasksComponent 
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent
+            }
+        ]
     }
 ];
--- 14-starting-project/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-starting-project/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,16 +1,34 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, OnInit, DestroyRef } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink],
 })
-export class UserTasksComponent {
-  userId = input.required<string>(); // import it in app.config
+export class UserTasksComponent implements OnInit{
+  // userId = input.required<string>(); // import it in app.config
   private usersService = inject(UsersService);
-  userName = computed(
-    () => this.usersService.users.find(u => u.id === this.userId())?.name
-  );
+  
+  private activatedRoute = inject(ActivatedRoute); // alternative approach
+  private destroyRef = inject(DestroyRef);
+  userName = '';
+
+  // userName = computed(
+  //   () => this.usersService.users.find(u => u.id === this.userId())?.name
+  // );
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+    const subscription = this.activatedRoute.params.subscribe({
+      next: paramMap => {
+        this.userName = this.usersService.users.find(u => u.id === paramMap['userId'])
+          ?.name || '';
+      }
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe())
+  }
 }
