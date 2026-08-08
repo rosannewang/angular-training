81452de79c69807d036a501a77028ec340862a6b
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-08 01:01:27 -0400

    Version 14.284: Extract user child routes to separate route file

--- 14-starting-project/src/app.routes.ts
+++ 14-starting-project/src/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
+
+import { routes as userRoutes } from './app/users/users.routes';
 import { UserTasksComponent } from './app/users/user-tasks/user-tasks.component';
 import { NoTaskComponent } from './app/tasks/no-task/no-task.component';
-import { TasksComponent } from './app/tasks/tasks.component';
-import { NewTaskComponent } from './app/tasks/new-task/new-task.component';
 import { NotFoundComponent } from './app/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -13,21 +13,7 @@ export const routes: Routes = [
     { 
         path: 'users/:userId', // your-domain/users/uid
         component: UserTasksComponent, // add child router in user-tasks html
-        children: [
-            {
-                path: '', // redirect to show all tasks
-                redirectTo: 'tasks',
-                pathMatch: 'prefix'
-            },
-            { 
-                path: 'tasks', // your-domain/users/uid/tasks
-                component: TasksComponent 
-            },
-            {
-                path: 'tasks/new',
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes
     },
     {
         path: '**', // fallback route - matches any route that doesn't match the above
