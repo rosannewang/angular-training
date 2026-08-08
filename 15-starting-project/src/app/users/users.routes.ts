88211c148b7ed99f80116361bcdfb7570281d2ea
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-08 14:13:34 -0400

    Version 15.306: Add route providers for service injection and move resolver to component

--- 15-starting-project/src/app/users/users.routes.ts
+++ 15-starting-project/src/app/users/users.routes.ts
@@ -1,52 +1,35 @@
 import { Routes } from '@angular/router';
 
-import { TasksComponent } from '../tasks/tasks.component';
+import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
-import { Task } from '../tasks/task/task.model';
-import { ResolveFn } from '@angular/router';
 import { TasksService } from '../tasks/tasks.service';
-import { inject } from '@angular/core';
-
-const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
-
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
+    providers: [TasksService], // all nested routes will have access
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () => 
+        //   import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
   },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    loadComponent: () => 
-      import('../tasks/tasks.component').then(mod => mod.TasksComponent),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
-  },
+
 ];
