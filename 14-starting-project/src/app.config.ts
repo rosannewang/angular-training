bdea0d1fbfd6d10337bb61b3b8cbfba377e86da8
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-08 00:20:08 -0400

    Version 14.279-14.280: Add params inheritance strategy for route parameter passing and filter tasks by userId

--- 14-starting-project/src/app.config.ts
+++ 14-starting-project/src/app.config.ts
@@ -1,9 +1,15 @@
-import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router";
 import { routes } from "./app.routes";
 import { ApplicationConfig } from "@angular/core";
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes, withComponentInputBinding()),
+        provideRouter(
+            routes, 
+            withComponentInputBinding(), 
+            withRouterConfig({ 
+                paramsInheritanceStrategy: 'always' // ensures dynamic path values are injected into child routes
+            })   
+        ),
     ]
 };
