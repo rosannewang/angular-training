05ef0d6b83b0a25cc710d3b6ab1f73d6a987a2ef
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-08 14:05:39 -0400

    Version 15.304-15.305: Implement lazy loading with loadChildren and loadComponent

--- 15-starting-project/src/app/users/users.routes.ts
+++ 15-starting-project/src/app/users/users.routes.ts
@@ -1,7 +1,33 @@
 import { Routes } from '@angular/router';
 
-import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
+import { TasksComponent } from '../tasks/tasks.component';
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { Task } from '../tasks/task/task.model';
+import { ResolveFn } from '@angular/router';
+import { TasksService } from '../tasks/tasks.service';
+import { inject } from '@angular/core';
+
+const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
+
 
 export const routes: Routes = [
   {
@@ -11,7 +37,8 @@ export const routes: Routes = [
   },
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
+    loadComponent: () => 
+      import('../tasks/tasks.component').then(mod => mod.TasksComponent),
     runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
