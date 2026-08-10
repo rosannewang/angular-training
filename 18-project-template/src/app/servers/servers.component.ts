ed59041b8dd70d7da88bf122eb6f2234a2debf0e
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-10 00:07:08 -0400

    Version 18.334-18.344: Understanding Components, Selectors, Templates & Styling

---
+++ 18-project-template/src/app/servers/servers.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
