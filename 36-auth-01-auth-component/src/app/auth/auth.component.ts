b893da79b667aa7890ef764ddfaa0bcc1de9f5a5
Rosanne Wang <rosanne7wang@gmail.com>
2026-08-10 21:28:49 -0400

    Version 36.615-36.617: Add template-driven form with validation

--- 36-auth-01-auth-component/src/app/auth/auth.component.ts
+++ 36-auth-01-auth-component/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -13,4 +14,8 @@ export class AuthComponent{
     this.isLoginMode = !this.isLoginMode;
   }
 
+  onSubmit(form: NgForm){
+    console.log(form.value);
+    form.reset();
+  }
 }
